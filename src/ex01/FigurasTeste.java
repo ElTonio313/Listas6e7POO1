2f1918f820ae0d9065a32ef64af02ea77484519a
Tony <antonyrinaldisikorski@gmail.com>
2025-11-29 14:49:49 -0300

    add do-while

--- src/ex01/FigurasTeste.java
+++ src/ex01/FigurasTeste.java
@@ -7,33 +7,37 @@ public class FigurasTeste {
 
         Scanner input = new Scanner(System.in);
         FigurasGeometricas figura = null;
+        int escolha = 1;
 
-        
-        System.out.println("Escolha a figura geométrica (1- Quadrado, 2- Retângulo, 3- Losango, 4 - Trapézio, 5 - Círculo): ");
-        int escolha = input.nextInt();
+        do {
+            System.out.println("Escolha a figura geométrica (1- Quadrado, 2- Retângulo, 3- Losango, 4 - Trapézio, 5 - Círculo, 6 - Sair): ");
+            escolha = input.nextInt();
 
-        switch (escolha) {
-            case 1:
-                figura = new Quadrado("Azul", 5);
-                break;
-            case 2:
-                figura = new Retangulo("Vermelho", 4, 6);
-                break;
-            case 3:
-                figura = new Losango("Verde", 8, 6, 5);
-                break;
-            case 4:
-                figura = new Trapezio("Amarelo", 10, 6, 4, 5, 5);
-                break;
-            case 5:
-                figura = new Circulo("Roxo", 7);
-                break;
-            default:
-                break;
-        }
+            switch (escolha) {
+                case 1:
+                    figura = new Quadrado("Azul", 5);
+                    break;
+                case 2:
+                    figura = new Retangulo("Vermelho", 4, 6);
+                    break;
+                case 3:
+                    figura = new Losango("Verde", 8, 6, 5);
+                    break;
+                case 4:
+                    figura = new Trapezio("Amarelo", 10, 6, 4, 5, 5);
+                    break;
+                case 5:
+                    figura = new Circulo("Roxo", 7);
+                    break;
+                default:
+                    break;
+            }
 
-        System.out.println("Área da figura: " + figura.calcularArea());
-        System.out.println("Perímetro da figura: " + figura.calcularPerimetro());
-    
+            if (escolha < 6 && escolha > 0) {
+                System.out.println("Área da figura: " + figura.calcularArea());
+                System.out.println("Perímetro da figura: " + figura.calcularPerimetro());
+            }
+        } while (escolha < 6 && escolha > 0);
+
     }
-}   
+}
